d96ae18b42697aad0d6f6c603a5504a93f8d2f77
Rubina1978 <hrubesova@gmail.com>
2026-01-08 22:22:43 +0000

    add images

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+const cardArray = [
+    {
+        name: 'Baby Metal',
+        img: images/babymetal.jpg,
+    },
+    
+    {
+        name: 'Korn',
+        img: images/korn.jpg,
+    },
+    {
+        name: 'Limp Bizkit',
+        img: images/limpbizkit.webp,
+    },
+    {
+        name: 'Pantera',
+        img: images/pantera.webp,
+    },
+
+    {
+        name: 'Rammstein',
+        img: images/Rammstein_-_Sehnsucht.jpg,
+    },
+    {
+        name: 'Slayer',
+        img: images/slayer.webp,
+    },
+    {
+        name: 'Slipknot',
+        img: images/slipknot1.jpg,
+    },
+    {
+        name: 'System of Down',
+        img: images/systemofdown.webp,
+    },
+    {
+        name: 'Baby Metal',
+        img: images/babymetal.jpg,
+    },
+    
+    {
+        name: 'Korn',
+        img: images/korn.jpg,
+    },
+    {
+        name: 'Limp Bizkit',
+        img: images/limpbizkit.webp,
+    },
+    {
+        name: 'Pantera',
+        img: images/pantera.webp,
+    },
+
+    {
+        name: 'Rammstein',
+        img: images/Rammstein_-_Sehnsucht.jpg,
+    },
+    {
+        name: 'Slayer',
+        img: images/slayer.webp,
+    },
+    {
+        name: 'Slipknot',
+        img: images/slipknot1.jpg,
+    },
+    {
+        name: 'System of Down',
+        img: images/systemofdown.webp,
+    },
+
+    
+]
