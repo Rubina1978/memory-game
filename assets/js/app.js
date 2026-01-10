86a45b04758ef2e0e8518f4745e74542844ee46b
Rubina1978 <hrubesova@gmail.com>
2026-01-10 17:28:27 +0000

    create grid according to level and number of cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,132 @@
+
 const cardArray = [
     {
         name: 'Baby Metal',
-        img: images/babymetal.jpg,
+        img: 'images/babymetal.jpg',
     },
     
     {
         name: 'Korn',
-        img: images/korn.jpg,
+        img: 'images/korn.jpg',
     },
     {
         name: 'Limp Bizkit',
-        img: images/limpbizkit.webp,
+        img: 'images/limpbizkit.webp',
     },
     {
         name: 'Pantera',
-        img: images/pantera.webp,
+        img: 'images/pantera.webp',
     },
 
     {
         name: 'Rammstein',
-        img: images/Rammstein_-_Sehnsucht.jpg,
+        img: 'images/Rammstein_-_Sehnsucht.jpg',
     },
     {
         name: 'Slayer',
-        img: images/slayer.webp,
+        img: 'images/slayer.webp',
     },
     {
         name: 'Slipknot',
-        img: images/slipknot1.jpg,
+        img: 'images/slipknot1.jpg',
     },
     {
         name: 'System of Down',
-        img: images/systemofdown.webp,
+        img: 'images/systemofdown.webp',
     },
     {
         name: 'Baby Metal',
-        img: images/babymetal.jpg,
+        img: 'images/babymetal.jpg',
     },
     
     {
         name: 'Korn',
-        img: images/korn.jpg,
+        img: 'images/korn.jpg',
     },
     {
         name: 'Limp Bizkit',
-        img: images/limpbizkit.webp,
+        img: 'images/limpbizkit.webp',
     },
     {
         name: 'Pantera',
-        img: images/pantera.webp,
+        img: 'images/pantera.webp',
     },
 
     {
         name: 'Rammstein',
-        img: images/Rammstein_-_Sehnsucht.jpg,
+        img: 'images/Rammstein_-_Sehnsucht.jpg',
     },
     {
         name: 'Slayer',
-        img: images/slayer.webp,
+        img: 'images/slayer.webp',
     },
     {
         name: 'Slipknot',
-        img: images/slipknot1.jpg,
+        img: 'images/slipknot1.jpg',
     },
     {
         name: 'System of Down',
-        img: images/systemofdown.webp,
+        img: 'images/systemofdown.webp',
     },
-
-    
+  
 ]
+
+
+/*shuffle formula*/
+cardArray.sort(() => 0.5 - Math.random())
+
+
+/*selecting levels*/
+function SelectLevel() {
+    document.querySelector('#level1').addEventListener('click', () => {
+        createBoard(8)
+    })
+    document.querySelector('#level2').addEventListener('click', () => {
+        createBoard(12)
+    })
+    document.querySelector('#level3').addEventListener('click', () => {
+        createBoard(16)
+    })
+}
+SelectLevel()
+
+const gridDisplay = document.querySelector('#grid')
+
+/*creating columns according to level and number of cards*/
+function getColumns(numCards) {
+   
+    if (numCards === 8) return 4
+    if (numCards === 12) return 4
+    if (numCards === 16) return 4
+}
+
+
+function createBoard (numCards) {
+    gridDisplay.innerHTML = ''
+    
+    const columns = getColumns(numCards)
+    gridDisplay.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+
+    cardArray.slice(0, numCards).forEach((_, index) => {
+      const card = document.createElement('img')
+      card.setAttribute('src', 'images/card-back.jpg')
+      card.setAttribute('data-id', index)
+      gridDisplay.appendChild(card)
+      
+    })
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
