21b344f28a04141c524b5996e6bd818f59347483
Rubina1978 <hrubesova@gmail.com>
2026-01-30 01:14:24 +0000

    Add function to flip card and checkmatch  plus rewrite code logic to show pairs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 
-const cardArray = [
+const baseCards = [
     {
         name: 'Baby Metal',
         img: 'images/babymetal.jpg',
@@ -34,46 +34,50 @@ const cardArray = [
         name: 'System of Down',
         img: 'images/systemofdown.webp',
     },
-    {
-        name: 'Baby Metal',
-        img: 'images/babymetal.jpg',
-    },
+    // {
+    //     name: 'Baby Metal',
+    //     img: 'images/babymetal.jpg',
+    // },
     
-    {
-        name: 'Korn',
-        img: 'images/korn.jpg',
-    },
-    {
-        name: 'Limp Bizkit',
-        img: 'images/limpbizkit.webp',
-    },
-    {
-        name: 'Pantera',
-        img: 'images/pantera.webp',
-    },
+    // {
+    //     name: 'Korn',
+    //     img: 'images/korn.jpg',
+    // },
+    // {
+    //     name: 'Limp Bizkit',
+    //     img: 'images/limpbizkit.webp',
+    // },
+    // {
+    //     name: 'Pantera',
+    //     img: 'images/pantera.webp',
+    // },
 
-    {
-        name: 'Rammstein',
-        img: 'images/Rammstein_-_Sehnsucht.jpg',
-    },
-    {
-        name: 'Slayer',
-        img: 'images/slayer.webp',
-    },
-    {
-        name: 'Slipknot',
-        img: 'images/slipknot1.jpg',
-    },
-    {
-        name: 'System of Down',
-        img: 'images/systemofdown.webp',
-    },
+    // {
+    //     name: 'Rammstein',
+    //     img: 'images/Rammstein_-_Sehnsucht.jpg',
+    // },
+    // {
+    //     name: 'Slayer',
+    //     img: 'images/slayer.webp',
+    // },
+    // {
+    //     name: 'Slipknot',
+    //     img: 'images/slipknot1.jpg',
+    // },
+    // {
+    //     name: 'System of Down',
+    //     img: 'images/systemofdown.webp',
+    // 
   
 ]
 
 
-/*shuffle formula*/
-cardArray.sort(() => 0.5 - Math.random())
+
+/*array created to put inside selected card*/
+const cardSelected = []
+
+/*array for chosen cards to be push into*/
+const cardsChosenIds = []
 
 
 /*selecting levels*/
@@ -100,21 +104,78 @@ function getColumns(numCards) {
     if (numCards === 16) return 4
 }
 
+function buildCardArray(numCards) {
+  const pairsNeeded = numCards / 2
+
+  const selected = baseCards
+    .sort(() => 0.5 - Math.random())
+    .slice(0, pairsNeeded)
+
+  return [...selected, ...selected]
+    .sort(() => 0.5 - Math.random())
+}
+
+
 
 function createBoard (numCards) {
+    /*clear screen*/
     gridDisplay.innerHTML = ''
-    
+
+    /*create columns*/
     const columns = getColumns(numCards)
     gridDisplay.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
 
-    cardArray.slice(0, numCards).forEach((_, index) => {
+    cardArray = buildCardArray(numCards) // ← NEW LINE
+
+    const levelCards = cardArray
+        .slice(0, numCards)
+        /*shuffle formula*/
+        .sort(() => 0.5 - Math.random())
+
+
+    cardArray.forEach((_, index) => {
       const card = document.createElement('img')
       card.setAttribute('src', 'images/card-back.jpg')
       card.setAttribute('data-id', index)
+      card.addEventListener('click', flipCard)
       gridDisplay.appendChild(card)
       
     })
   
+}
+
+function checkMatch() {
+    /*search for all images in the entire document*/
+    const cards = document.querySelectorAll('#grid img')
+    console.log(cards)
+
+    /*get cards to the card array and check if they match*/
+   if (cardSelected[0] === cardSelected[1]) {
+    alert('this is a match!')
+    cards(cardsChosenIds)[0].setAttribute('src', 'images/white.png')
+   }
+}
+       
+ 
+function flipCard() {
+    /*on click get data id, this means whichever is clicked it will interact with*/
+    const cardId = this.getAttribute('data-id')
+
+    /* picking the name of the card*/
+    // cardArray[cardId].name
+    cardSelected.push(cardArray[cardId].name)
+
+    /*push cards id to the array*/
+    cardsChosenIds.push(cardId)
+
+    /* show (add image)*/
+    this.setAttribute('src', cardArray[cardId].img)
+    /*check for match*/
+    if (cardSelected.length === 2) {
+        setTimeout(checkMatch, 500)
+         
+    }
+
 }
 
 
